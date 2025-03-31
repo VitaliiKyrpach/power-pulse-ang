687c600eba095223391a96e2a4ac2e7991e8c841
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2025-03-31 09:42:06 +0300

    start new petProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IconSpriteModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
