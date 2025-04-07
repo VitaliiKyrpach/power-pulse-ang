ce85b08200a393ad3c6994007a7179d2f120d215
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2025-04-07 11:32:27 +0300

    setting up some routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
 import { HeaderComponent } from './components/header/header.component';
+import { filter } from 'rxjs';
+import { StatisticComponent } from './components/statistic/statistic.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IconSpriteModule, HeaderComponent],
+  imports: [RouterOutlet, IconSpriteModule, HeaderComponent, StatisticComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'power-pulse-ang';
+  public place: string = '';
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter(event=> event instanceof NavigationEnd)).subscribe(event=>{
+      this.place = event.url.replace('/', '')
+    })
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +10,9 @@ import { IconSpriteModule } from 'ng-svg-icon-sprite';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+constructor(private router: Router){}
+  ngOnInit(): void {
+  this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(event=> console.log(event.url.replace('/', '')))
+}
 }
