a5206d2324745765e1561a92d72ee03f5af6529b
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2025-04-15 11:20:51 +0300

    add productsPage and fix some styles

--- src/app/components/products-page/products-page.component.ts
+++ src/app/components/products-page/products-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 @Component({
   selector: 'app-products-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, IconSpriteModule],
   templateUrl: './products-page.component.html',
-  styleUrl: './products-page.component.scss'
+  styleUrl: './products-page.component.scss',
 })
 export class ProductsPageComponent {
-
+  public dataExist: boolean = false;
+  public search: string = '';
+  public clearSearch(): void {
+    this.search = '';
+  }
+  public goSearch(): void {
+    console.log(this.search);
+  }
 }
